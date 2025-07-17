a5d1facc0ddb18c57e7ef0cf0eceda5d686f602b
Eng-Yhassan <muscabhassan81@gmail.com>
2025-07-17 06:56:03 +0300

    add

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Hambarger Menu Bar
 const Dropdown=document.querySelector("#Dropdown");
 const Dropdown_pages=document.querySelector("#Dropdown_pages");
 const x_mark=document.querySelector("#x_mark");
@@ -11,4 +12,18 @@ x_mark.addEventListener("click",()=>{
     Dropdown_pages.style.display= 'none';
     Dropdown.style.display= 'block';
     x_mark.style.display= 'none'
+})
+
+
+// Cart_incriment
+const Cart_incriment=document.querySelector("#Cart_incriment");
+const Cart_incriment_Drop=document.querySelector("#Cart_incriment_Drop");
+const Explore_More=document.querySelector("#Explore_More");
+
+let incriment= 0
+
+Explore_More.addEventListener('click',()=>{
+    incriment ++
+    Cart_incriment.innerHTML= `(${incriment})`;
+    Cart_incriment_Drop.innerHTML= `(${incriment})`;
 })
